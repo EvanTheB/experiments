3c20cac790e63263cab60a34f375e88c86be8ba3
Evan Benn <evanbenn@gmail.com>
2019-08-02 17:52:24 +1000

    When do cpp constructors fire

---
+++ cpp_constructors/const1.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+struct X {
+    int i{2};
+
+    X() {
+        std::cout << "default constructor called" << std::endl;
+    }
+
+    X(const X& other) {
+        std::cout << "copy constructor called" << std::endl;
+    }
+    X& operator=(const X& other) {
+        std::cout << "copy assignment called" << std::endl;
+    }
+    
+    X(X&&) {
+       std::cout << "move constructor called" << std::endl;
+    }
+    X& operator=(X&& other) {
+        std::cout << "move assignment called" << std::endl;
+    }
+};
+
+X createX() {
+    X x;
+    std::cout << "created x on the stack" << std::endl;
+    return x;
+}
+
+int main() {
+    X x1;
+    std::cout << "created x1" << std::endl;
+    std::cout << "x1: " << x1.i << std::endl << std::endl;    
+
+    X x2 = createX();
+    std::cout << "created x2" << std::endl;
+    std::cout << "x2: " << x2.i << std::endl;    
+
+    return 0;
+}
